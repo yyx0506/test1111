e7e7a6938bdc15d82622d50dbe502b898a6a9f5a
yyx0506 <1138394870@qq.com>
2019-07-02 18:00:33 +0800

    djnago项目的创建，初始操作

--- demo0/demo1/booktest/models.py
+++ demo0/demo1/booktest/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Bookinfo(models.Model):
+    title=models.CharField(max_length=20)
+    pub_date=models.DateTimeField(auto_now=True)
+
+
+class HeroInfo(models.Model):
+    name=models.CharField(max_length=20)
+    gender=models.BooleanField(default=True)
+    content=models.CharField(max_length=100)
+    book=models.ForeignKey(Bookinfo,on_delete=models.CASCADE)
