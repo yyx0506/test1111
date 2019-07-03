fe4df72459b54238330fde106ac82ab7988a10fa
yyx0506 <1138394870@qq.com>
2019-07-03 21:36:01 +0800

    20190703晚项目练习

---
+++ demo0/demo2/newbook/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from .models import Newbook
+def index(request):
+    return render(request,"newbook/index.html",{"username":"杨超越"})
+def list(request):
+    books = Newbook.objects.all()
+    return render(request,"newbook/list.html",{"books":books})
+def detail(request,id):
+    book = Newbook.objects.get(pk=id)
+    return render(request,"newbook/detail.html",{"book":book})
