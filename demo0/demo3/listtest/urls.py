7140da82082543c47d53d6b18f7e0320e4538cf5
yyx0506 <1138394870@qq.com>
2019-07-05 11:48:07 +0800

    20190705投票更新内容

--- demo0/demo3/listtest/urls.py
+++ demo0/demo3/listtest/urls.py
@@ -5,7 +5,8 @@ from . import views
 app_name='listtest'
 urlpatterns=[
     url(r'^$', views.index, name='index'),
-    url(r'^index/(\d+)/$', views.list, name='list'),
+    url(r'^list/(\d+)/$', views.list, name='list'),
     url(r'^result/(\d+)/$', views.result, name='result'),
     url(r'^newquestion/$',views.newquestion,name='newquestion'),
+    url(r'^addchouse/(\d+)/$',views.addchouse,name='addchouse')
 ]
